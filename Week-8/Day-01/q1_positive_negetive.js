dc4f0dc9ba756c3050ac8052317083f1b1bb8081
Pooja Kanwar <pooja.kanwar.rathore6980@gmail.com>
2025-08-18 15:19:38 +0530

    Added day 1 of week 8

---
+++ Week-8/Day-01/q1_positive_negetive.js
@@ -0,0 +1,10 @@
+// Program to check if a number is positive, negative, or zero
+
+let number = 5
+if (number > 0) {
+    console.log(" positive.");
+} else if (number < 0) {
+    console.log("negative.");
+} else {
+    console.log(" zero.");
+}
